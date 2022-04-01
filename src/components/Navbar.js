9a9b7c7fd0d36fe2c99c8cf4d6090383a8abf0b5
kalkin-kap <ryanroams123@gmail.com>
2022-04-01 19:58:56 +0530

    initialise project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,11 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-1 col-md-2 mr-0"
+        <h4
           style={{ color: 'white' }}
         >
           EthSwap
-        </a>
+        </h4>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
